a5a3ef95233a7c82a15d4b6a81dc39ce419d5d00
GitHub-LiuMing <GoogLiuMing@gmail.com>
2023-02-12 21:18:42 +0800

    Update FileListener.java

--- src/main/java/cn/com/cnpc/clouddata/filelistener/FileListener.java
+++ src/main/java/cn/com/cnpc/clouddata/filelistener/FileListener.java
@@ -126,4 +126,8 @@ public class FileListener extends FileAlterationListenerAdaptor {
         // TODO Auto-generated method stub
         super.onStart(observer);
     }
+    public void onStop(FileAlterationObserver observer) {
+        // TODO Auto-generated method stub
+        super.onStop(observer);
+    }
 }
