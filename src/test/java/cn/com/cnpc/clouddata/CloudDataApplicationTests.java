25b7aa8f3c67f9f4d87843e6d2e2b375e5ead52d
GitHub-LiuMing <GoogLiuMing@gmail.com>
2024-03-04 22:11:20 +0800

    Update CloudDataApplicationTests.java

--- src/test/java/cn/com/cnpc/clouddata/CloudDataApplicationTests.java
+++ src/test/java/cn/com/cnpc/clouddata/CloudDataApplicationTests.java
@@ -206,4 +206,31 @@ class CloudDataApplicationTests {
      return list;
      }
      */
+
+    //无对象的写入
+     private List<List<String>> head() {
+     List<List<String>> list = new ArrayList<>();
+     List<String> head0 = new ArrayList<>();
+     head0.add("姓名");
+     List<String> head1 = new ArrayList<>();
+     head1.add("年龄");
+     List<String> head2 = new ArrayList<>();
+     head2.add("生日");
+     list.add(head0);
+     list.add(head1);
+     list.add(head2);
+     return list;
+     }
+
+     private List<List<Object>> dataList() {
+     List<List<Object>> list = new ArrayList<>();
+     for (int i = 0; i < 10; i++) {
+     List<Object> data = new ArrayList<>();
+     data.add("张三");
+     data.add(25);
+     data.add(new Date());
+     list.add(data);
+     }
+     return list;
+     }
 }
